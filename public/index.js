30999a962ecd5e9ec68c4ef579939defa825cce6
imdp <duncan@dinegigs.com>
2021-10-28 14:58:00 -0400

    first commit

---
+++ public/index.js
@@ -0,0 +1,33 @@
+function Spa() {
+    return (
+        <HashRouter>
+            <div>
+                <NavBar />
+                <UserContext.Provider value={{
+                    users:
+                        [{
+                            name: 'bill',
+                            email: 'bill@gatesfoundation.com',
+                            password: 'vaxmoneyboi2020'
+                        }]
+                }}>
+
+                    <div className="container" style={{ padding: "20px" }}>
+                        <Route path="/" exact component={Home} />
+                        <Route path="/CreateAccount/" component={CreateAccount} />
+                        <Route path="/alldata/" component={AllData} />
+                        <Route path="/login/" component={Login} />
+                        <Route path="/deposit/" component={Deposit} />
+                        <Route path="/withdraw/" component={Withdraw} />
+                        <Route path="/balance/" component={Balance} />
+                    </div>
+                </UserContext.Provider>
+            </div>
+        </HashRouter>
+    )
+}
+
+ReactDOM.render(<Spa />,
+    document.getElementById('root')
+);
+
